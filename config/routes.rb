676f23ab4c2c1b5b545788c970f539dd2e1f3d6c
yachnytskyi <yachnytskyi1992@gmail.com>
2018-09-20 11:45:28 +0300

    Added scaffold Channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :discussions
+  resources :channels
+  resources :discussions do
+    resources :replies
+  end
   root 'home#index'
 
   devise_for :users, controllers: { registrations: 'registrations' }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_082253) do
+ActiveRecord::Schema.define(version: 2018_09_20_084259) do
+
+  create_table "channels", force: :cascade do |t|
+    t.string "channel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "discussions", force: :cascade do |t|
     t.string "title"
@@ -18,6 +24,12 @@ ActiveRecord::Schema.define(version: 2018_09_20_082253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.text "reply"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
