d4147f21def6f797791a0c421698d8204cd22554
yachnytskyi <yachnytskyi1992@gmail.com>
2018-09-20 11:23:04 +0300

    Added migration user_id_to_discussions.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_081349) do
+ActiveRecord::Schema.define(version: 2018_09_20_082253) do
 
   create_table "discussions", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
