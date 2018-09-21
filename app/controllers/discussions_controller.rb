7e348947f61199a225a1fc1d39e116abdf7193e3
yachnytskyi <yachnytskyi1992@gmail.com>
2018-09-21 13:11:52 +0300

    fixed discussions_controller and modifed discussions show.html.erb

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -76,6 +76,6 @@ class DiscussionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def discussion_params
-      params.require(:discussion).permit(:title, :content)
+      params.require(:discussion).permit(:title, :content, :channel_id)
     end
 end
